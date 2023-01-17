82ed9e4b250283f45f55db12089065b9cdc1bea2
Piotr Ludwiczuk <plu@schultz.dk>
2023-01-17 14:45:10 +0100

    Table storage

---
+++ TableStorage.ps1
@@ -0,0 +1,11 @@
+Install-Module AzTable
+Import-Module AzTable
+
+Add-AzAccount
+
+$account = Get-AzStorageAccount -ResourceGroupName "PiotrResourceGroup" -Name "nowyswiat"
+$account
+
+$table = (Get-AzStorageTable -Context $account.Context -Name "NumberOfPatrons").CloudTable
+
+Get-AzTableRow -table $table -PartitionKey "partition1" | Select-Object NoOfPatrons, RowKey, TotalAmount, MonthlyAmount | Format-Table
