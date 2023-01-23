63ca8c694cc17c9d82e55a767362793d427b893f
Piotr Ludwiczuk <plu@schultz.dk>
2023-01-23 16:39:33 +0100

    Az.Redis

---
+++ Az.Redis/Az.Redis/Program.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using static System.Console;
+
+var connectionString = "";
+
+await using var cache = ConnectionMultiplexer.Connect(connectionString);
+IDatabase db = cache.GetDatabase();
+
+
+// Snippet below executes a PING to test the server connection
+var result = await db.ExecuteAsync("ping");
+WriteLine($"PING = {result.Type} : {result}");
+
+// Call StringSetAsync on the IDatabase object to set the key "test:key" to the value "100"
+bool setValue = await db.StringSetAsync("test:key", "100");
+WriteLine($"SET: {setValue}");
+
+// StringGetAsync takes the key to retrieve and return the value
+string? getValue = await db.StringGetAsync("test:key");
+
+WriteLine($"GET: {getValue}");
