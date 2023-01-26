e09015373594a0b86e19a7b586d35b9d9b3b9588
Piotr Ludwiczuk <plu@schultz.dk>
2023-01-26 16:50:54 +0100

    csv

--- TableStorage.ps1
+++ TableStorage.ps1
@@ -6,4 +6,5 @@ Add-AzAccount
 $account = Get-AzStorageAccount -ResourceGroupName "PiotrResourceGroup" -Name "nowyswiat"
 $account
 $table = (Get-AzStorageTable -Context $account.Context -Name "NumberOfPatrons").CloudTable
-Get-AzTableRow -table $table -PartitionKey "partition1" | Select-Object NoOfPatrons, RowKey, TotalAmount, MonthlyAmount | Format-Table
+#Get-AzTableRow -table $table -PartitionKey "partition1" | Select-Object NoOfPatrons, RowKey, TotalAmount, MonthlyAmount | Format-Table
+Get-AzTableRow -table $table -PartitionKey "partition1" | Select-Object NoOfPatrons, RowKey, TotalAmount, MonthlyAmount | ConvertTo-Csv
