c7ec73f27680f7f2d862b9843d4407535ae30dca
Piotr Ludwiczuk <plu@schultz.dk>
2023-01-30 13:05:25 +0100

    plot

---
+++ NowySwiatStatsPlot/PlotRecord.cs
@@ -0,0 +1,16 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace NowySwiatStatsPlot
+{
+    public class PlotRecord
+    {
+        [Name(("RowKey"))]
+        public DateTime Date { get; set; }
+
+        public int NoOfPatrons { get; set; }
+
+        public int? MonthlyAmount { get; set;}
+
+        public int? TotalAmount { get; set;}
+    }
+}
---
+++ NowySwiatStatsPlot/Program.cs
@@ -0,0 +1,47 @@
+﻿using CsvHelper;
+using System.Globalization;
+using NowySwiatStatsPlot;
+using ScottPlot;
+
+using var reader = new StreamReader(@"C:\temp\stats.csv");
+using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+var records = csv.GetRecords<PlotRecord>().ToList();
+Console.WriteLine(records.Count());
+
+double[] noOfPatrons = records.Select(x => (double)x.NoOfPatrons).ToArray();
+double[] dates = records.Select(x => x.Date.ToOADate()).ToArray();
+var plotNoOfPatrons = new Plot(900, 500);
+
+plotNoOfPatrons.AddScatter(dates, noOfPatrons);
+plotNoOfPatrons.XAxis.DateTimeFormat(true);
+
+plotNoOfPatrons.Title("Number of patrons in 2023");
+plotNoOfPatrons.YAxis.Label("Number of patrons");
+
+plotNoOfPatrons.SaveFig(@"C:\temp\no-of-patrons.png");
+
+double[] monthlyAmount = records.Where(x => x.MonthlyAmount.HasValue).Select(x => (double)x.MonthlyAmount.Value).ToArray();
+double[] monthlyAmountDates = records.Where(x => x.MonthlyAmount.HasValue).Select(x => x.Date.ToOADate()).ToArray();
+
+var plotMonthlyAmount = new Plot(900, 500);
+
+plotMonthlyAmount.AddScatter(monthlyAmountDates, monthlyAmount);
+plotMonthlyAmount.XAxis.DateTimeFormat(true);
+
+plotMonthlyAmount.Title("Monthly amount in 2023");
+plotMonthlyAmount.YAxis.Label("Monthly amount");
+
+plotMonthlyAmount.SaveFig(@"C:\temp\monthly-amount.png");
+
+double[] totalAmount = records.Where(x => x.TotalAmount.HasValue).Select(x => (double)x.TotalAmount.Value).ToArray();
+double[] totalAmountDates = records.Where(x => x.TotalAmount.HasValue).Select(x => x.Date.ToOADate()).ToArray();
+
+var plotTotalAmount = new Plot(900, 500);
+
+plotTotalAmount.AddScatter(totalAmountDates, totalAmount);
+plotTotalAmount.XAxis.DateTimeFormat(true);
+
+plotTotalAmount.Title("Total amount in 2023");
+plotTotalAmount.YAxis.Label("Total amount");
+
+plotTotalAmount.SaveFig(@"C:\temp\total-amount.png");
